dfe8a1816b349bbcaeac1ee40e17b08f9c40bd83
Brian Downs <brian.downs@gmail.com>
2018-09-16 20:57:08 -0700

    update

--- briandowns/c/comments/api.c
+++ briandowns/c/comments/api.c
@@ -46,8 +46,9 @@ static int callback_get_all_comments(const struct _u_request *request, struct _u
         return 1;
     }
 
-    json_t *json_array = json_array();
-    json_array_append(json_array);
+    // json_t *json_array = json_array();
+    // json_array_append(json_array);
+    json_object json_body = json_body();
     json_object_set_new(json_body, "id", json_integer(entry->id)); 
     json_object_set_new(json_body, "name", json_string(entry->name));
     json_object_set_new(json_body, "email", json_string(entry->email));
